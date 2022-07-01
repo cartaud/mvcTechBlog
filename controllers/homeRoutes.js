f180d1b49267862b1d94ad22b43ee8d6dbbbdcec
Chadd <chadd_artaud@cox.net>
2022-07-01 11:34:38 -0700

    created user routes for login/logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,15 +4,15 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        const userData = await User.findAll({
+        const userData = await User.findAll({ //instead of loading user data here, we want to load all post on initial load
             attributes: { exclude: ['password'] },
             order: [['name', 'ASC']],
         });
 
-        const users = userData.map((project) => project.get({ plain: true}));
+        const users = userData.map((project) => project.get({ plain: true}));//chang users to post
 
         res.render('homepage', {
-            users,
+            users, //put post data here 
             logged_in: req.session.logged_in,
         });
     } catch (err) {
